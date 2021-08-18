abcbfbd9941ce7127860744c046b780e040b76d9
ann3dyy <89078922+ann3dyy@users.noreply.github.com>
2021-08-18 11:13:17 +0800

    Update DA_group3.py

--- DA_group3.py
+++ DA_group3.py
@@ -1,2 +1,6 @@
 print('testing')
 print("this better work or im gonna cry")
+#it works
+#but it only works if changes are made in github n not in pycharm if my theory isn't wrong
+#go to the git option in the top and click 'update project' and 'merge...'
+#by doing this, ur updating the pycharm to this current one
