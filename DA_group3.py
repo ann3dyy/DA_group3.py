ed4aa8c3df0510929277fdb433de5011cbea06d7
ann3dyy <89078922+ann3dyy@users.noreply.github.com>
2021-08-19 09:23:43 +0800

    Update DA_group3.py

--- DA_group3.py
+++ DA_group3.py
@@ -1,20 +1,6 @@
-print('testing')
-print("this better work or im gonna cry")
-#it works
-#but it only works if changes are made in github n not in pycharm if my theory isn't wrong
-#go to the git option in the top and click 'update project' and 'merge...'
-#by doing this, ur updating the pycharm to this current one
-#we chould do updates everytime we gotta test this so yeah
-#once yalls have seen this imma erase this
 
-#ill be following lab 10a word for word and see if it works pls work
-import socket;
+
 import requests
-url = "http://localhost:8080/"
-url_used = print("url:", url) #this will be the url of the website we will be using
-# uh ignore these^^ for a sec
-
-
 
 #testing to see if lab 10b works for this
 #performing get request 
