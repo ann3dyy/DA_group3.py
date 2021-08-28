10e72a4cbad5db934fb32e5e1b4940c744b8fe04
yjie01 <89171370+yjie01@users.noreply.github.com>
2021-08-28 15:12:02 +0800

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,24 @@
+import scrapy
+from scrapy.http.request import Request
+import unittest
+
+
+class BrickSetSpider(scrapy.Spider):
+    name = "brickset_spider"
+    start_urls = ["https://brickset.com/sets/year-2000"]
+
+    def start_requests(self):
+        headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; vc:48.0) Gecko/20100101 Firefox/48.0"}
+        for url in self.start_urls:
+            yield Request (url, headers=headers)
+
+
+
+    def parse(self, response):
+        SET_SELECTOR = ".set"
+        for brickset in response.css(SET_SELECTOR):
+
+            NAME_SELECTOR = "h1 ::text"
+            yield{
+                "name": brickset.css(NAME_SELECTOR).extract_first(),
+            }
