0cb1c1709e2b929be4d926dd3170a49c4537ff6c
ann3dyy <89078922+ann3dyy@users.noreply.github.com>
2021-08-31 20:00:20 +0800

    Update DA_group3.py

--- DA_group3.py
+++ DA_group3.py
@@ -1,85 +1,62 @@
-# testing to see if lab 10b works for this
-# performing get request
+
+# imports used in the script
+#import request is used for lab 10b, to ensure that we CAN do HTTP requests towards the website using python
+#without request, we cannot send HTTP requests to the website for our script to work
+#import unittest is required so that we can actually test whether our units and our softwware code works
 import requests
+import unittest
 
-# testing my option system
-while True:
-    print("Welcome to DA_group3")
-    print("\t *", "Option 1) Perform the get request on website")
-    print("\t *", "Option 2) View the status code")
-    print("\t *", "Option 3) View website header")
-    print("\t *", "Option 4) View modified header User-Agent")
-    answer = input("What would you like to see? Option: ")
-    if answer == "1":
-        testing = "https://brickset.com/sets/year-2000"
-        t = requests.get(testing)
-        print(t.text)
-        
-        if t.text == t.text:
-            n1 = input("Would you like to view anything else?(Y/N): ")
-            if n1 == "y" and "Y":
-                pass
-            if n1 == "n" and "N":
-                break
-          
+#performing get request
+testing = "https://brickset.com/sets/year-2000"
+t = requests.get(testing)
+print(t.text)
 
-# testinng for OK return status
-    if answer == "2":
-        testing = "https://brickset.com/sets/year-2000"
-        t = requests.get(testing)
-        print("Status Code: ")
-        print("\t *", t.status_code)
-        if t.status_code == 200:
-            print("Translation: 200 = OK")
-            
-            if t.status_code == t.status_code:
-                n2 = input("Would you like to view anything else?(Y/N): ")
-                if n2 == "y" and "Y":
-                    pass
-                if n2 == "n" and "N":
-                    break
+#doing the okay status
+print("Status Code: ")
+print("\t *", t.status_code)
+if t.status_code == 200:
+    print("Translation: 200 = OK")
 
-            
-# testing for website header
-    if answer == "3":
-        testing = "https://brickset.com/sets/year-2000"
-        t = requests.get(testing)
-        h = requests.head(testing)
-        print("HEADER:")
-        print("*******************************************")
-        for x in h.headers:
-            print("\t *", x, ":", h.headers[x])
-        print("*******************************************")
-        
-        n3 = input("Would you like to view anything else?(Y/N): ")
-        if n3 == "y" and "Y":
-            pass
-        if n3 == "n" and "N":
-            break
-        
-  
-   
+#getting the header
+h = requests.head(testing)
+print("HEADER:")
+print("*******************************************")
+for x in h.headers:
+    print("\t *", x, ":", h.headers[x])
+print("*******************************************")
 
-        
-# testing for mobile for header user-agent
-    if answer == "4":
-        headers = {
+#attempting the user-agent
+headers = {
             "User-Agent": "Mobile"
-        }
-        testing2 = "https://brickset.com/sets/year-2000"
-        rh = requests.get(testing2, headers=headers)
-        print(rh.text)
-        n4 = input("Would you like to view anything else?(Y/N): ")
-        if n4 == "y" and "Y":
-            pass
-        if n4 == "n" and "N":
-            break
-
-        
-    else:
-        if answer != "1" and answer != "2" and answer != "3" and answer != "4":
-            print("answer undefined. please try again.")
+}
+testing2 = "http://httpbin.org/headers"
+rh = requests.get(testing2, headers=headers)
+print(rh.text)
 
 
 
+#doing scrapy
+#without import scrapy we cannot use the modules within it, such as spider
+import scrapy
+from scrapy.http.request import Request
 
+
+class BrickSetSpider(scrapy.Spider):
+    name = "brickset_spider"
+    start_urls = ["https://brickset.com/sets/year-2000"]
+
+    def start_requests(self):
+        headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; vc:48.0) Gecko/20100101 Firefox/48.0"}
+        for url in self.start_urls:
+            yield Request (url, headers=headers)
+
+
+
+    def parse(self, response):
+        SET_SELECTOR = ".set"
+        for brickset in response.css(SET_SELECTOR):
+
+            NAME_SELECTOR = "h1 ::text"
+            yield{
+                "name": brickset.css(NAME_SELECTOR).extract_first(),
+            }
